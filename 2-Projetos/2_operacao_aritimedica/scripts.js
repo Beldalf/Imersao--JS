31df986578b729e74c1a6eb54503da84a2803237
Beorm <abruquerque@gmail.com>
2026-08-22 20:26:16 -0300

    Adiciona os arquivos

--- 2-Projetos/2_operacao_aritimedica/scripts.js
+++ 2-Projetos/2_operacao_aritimedica/scripts.js
@@ -1,7 +1,7 @@
-console.log(2 + 3);
-console.log(10 - 5);
-console.log(4 * 6);
-console.log(15 / 3);
-console.log(20 % 2);
-console.log(21 % 2);
-console.log(2 ** 3);
+console.log(typeof (2 + 3));
+console.log(typeof (10 - 5));
+console.log(typeof (4 * 6));
+console.log(typeof (15 / 3));
+console.log(typeof (20 % 2));
+console.log(typeof (21 % 2));
+console.log(typeof (2 ** 3));
