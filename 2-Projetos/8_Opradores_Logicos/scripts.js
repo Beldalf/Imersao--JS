5cfed915625e0c30b73a9c6dbb2652aaa7ca182c
Beorm <abruquerque@gmail.com>
2026-08-26 13:03:47 -0300

    `Adiciona os Arquivos as Aulas`

---
+++ 2-Projetos/8_Opradores_Logicos/scripts.js
@@ -0,0 +1,82 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+
+console.log(3 == 3 && "Gabriel" == "Gabriel");
+console.log( "felipe" == "João" || false);
+
+console.log(!(!(true && true)));
+
+console.log(true && true);
+console.log(false || false);
+
+
+// Exercicios//
+console.log('_____EXERCICIOS_____');
+
+console.log( "guilherme" == "guilherme" &&  true);
+console.log("Gabriel" == "Guilherme" && false);
+console.log(10 < 20 || 20 > 10);
+console.log(20 < 10 || 10 > 20);
+console.log(10 > 20 || 20 > 10);
+console.log(!(64 > 58 && 91 >20));
+console.log(!(28 < 27 && 1 > 12));
+
+console.log("__________EXERCICIOS CHATGPT_________");
+
+console.log(true);
+console.log(false);
+
+let estachovendo = true;
+console.log(estachovendo);
+
+
+let temSol = false;
+console.log(temSol);
+
+console.log( 10 > 5);
+console.log(3 > 8);
+console.log(10 === 10);
+console.log(10 ==="10");
+
+console.log(20 < 30);
+console.log(50 >100);
+console.log(10 >= 10);
+console.log(9 <= 5);
+console.log(10 != 10);
+console.log(5 ==="5");
+console.log(5 =="5")
+
+console.log("_____TESTE_____");
+
+let idade = 20;
+let faixa = idade >= 18;
+console.log(faixa);
+
+let idade2 = 15;
+let faixa2 = idade2 >= 18;
+console.log(faixa2);
+
+let temcarta = true ;
+let dirige = idade + temcarta >= 18;
+console.log(dirige);
+
+let user = "Gabriel";
+let senha = "1234";
+
+let login = user == "Gabriel" && senha == "1234";
+console.log(login);
+
+let temperatura = 35 ;
+let clima = temperatura >= 30 || temperatura <= 10 ;
+console.log(clima);
+
+let idade3 = 22;
+let temIngresso = true;
+let acompanhado = false;
+
+let cinema = idade3 >= 18 && temIngresso || acompanhado;
+console.log(cinema);
+
+let teatro = temIngresso || acompanhado;
+console.log(teatro);
