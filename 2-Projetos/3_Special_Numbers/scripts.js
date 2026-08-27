7f1c7964c435f19a6831d5873bb0796f9b59f7a5
Beorm <abruquerque@gmail.com>
2026-08-27 12:56:29 -0300

    `Adiciona os Arquivos as Aulas`

--- 2-Projetos/3_Special_Numbers/scripts.js
+++ 2-Projetos/3_Special_Numbers/scripts.js
@@ -1,3 +1,15 @@
 console.log(typeof Infinity);
 console.log(typeof -Infinity);
 console.log(typeof NaN);
+
+// Exercicios//
+console.log("_____EXERCICIOS_____");
+
+// NaN
+console.log(0 / 0);
+
+// Infinity
+console.log(10 / 0);
+
+// -Infinity
+console.log(-10 / 0);
