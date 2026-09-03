870a261b5df7f6c42fbb56e9457fd079b45431dc
Beorm <abruquerque@gmail.com>
2026-09-03 12:44:00 -0300

    `Adiciona os Arquivo as Aulas`

---
+++ 3-Estrutura_de_Programação/11-else if/scripts.js
@@ -0,0 +1,8 @@
+let nome = "Gabriel";
+if (nome != undefined && nome == "Helio") {
+  console.log("Nome está definido");
+} else if (nome == "Gabriel" && nome.length > 7) {
+  console.log("O nome é Gabriel");
+} else {
+  console.log("Nome indefinido");
+}
--- 3-Estrutura_de_Programação/9-if/scripts.js
+++ 3-Estrutura_de_Programação/9-if/scripts.js
@@ -20,7 +20,7 @@ if ((nome == "Gabriel" && idade > 80) || passaport == true) {
 }
 
 //Execicios//
-
+console.log("_______EXERCICIOS________");
 if (idade > 18);
 {
   console.log("POde entrar na balada");
@@ -34,3 +34,98 @@ if (meuNome == "Gabriel");
 }
 
 //Execicios CHATGPT//
+// teste com if e else
+console.log("_______EXERCICIOSGPT________");
+{
+  let idadegpt = prompt("Digite sua Idade");
+
+  if (idadegpt > 18) {
+    console.log("é maior de idade");
+  } else {
+    console.log("Não é maior de idade");
+  }
+
+  let score = prompt("Digite o valo");
+  if (score > 0) {
+    console.log("Numero positivo");
+  } else {
+    console.log("Numero Negativo");
+  }
+
+  let imPar = prompt("Digite um numero");
+  if (imPar % 2 === 0) {
+    console.log("É Par");
+  } else {
+    console.log("É Impar");
+  }
+
+  if (idadegpt > 18) {
+    console.log("Pode dirigir");
+  } else {
+    console.log("Não pode dirigir");
+  }
+
+  let senha = prompt("Digite sua senha");
+  let senhaSalva = 1234;
+  if (senha == senhaSalva) {
+    console.log("Senha correta");
+  } else {
+    console.log("Senha incorreta");
+  }
+
+  // teste com else if
+
+  let nota = prompt("Insira a nota do Aluno");
+  if (nota > 7) {
+    console.log("Aprovado");
+  } else if (nota > 5 && nota < 6.9) {
+    console.log("Recuperação");
+  } else {
+    console.log("Reprovado");
+  }
+
+  let temperatura = prompt("Digite a temperatura");
+  if (temperatura > 30) {
+    console.log("Esta quente");
+  } else if (temperatura > 20 && temperatura < 30) {
+    console.log("Temperatura Agradavel");
+  } else {
+    console.log("Esta frio");
+  }
+
+  let aposta1 = prompt("Digite o primeiro valor"),
+    aposta2 = prompt("Digite o segundo valor"),
+    aposta3 = prompt("Digite o valor");
+
+  if (aposta1 > aposta2) {
+    console.log("O primeiro valor é Maior");
+  } else if (aposta1 == aposta2) {
+    console.log("Eles são iguais");
+  } else {
+    console.log("O segundo valor é maior");
+  }
+
+  let venda = prompt("Insira o valor da compra");
+  if (venda >= 100) {
+    console.log("Ganhou 10% de desconto");
+  } else {
+    console.log("Sem desconto");
+  }
+
+  let userCorreto = "admin";
+  let senhaCorreta = "1234";
+
+  if (userCorreto == "admin" && senhaCorreta == "1234") {
+    console.log("Bem vindo");
+  } else {
+    console.log("Usuario ou senha incorreta");
+  }
+
+  if (aposta1 > aposta2 > aposta3) {
+    console.log("O primeiro valor é Maior");
+  } else if (aposta1 < aposta3) {
+    console.log("O terceiro é Maior");
+  } else {
+    console.log("O segundo valor é maior");
+  }
+}
