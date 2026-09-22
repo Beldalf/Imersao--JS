1a1acdae670e7f8c8d67d7f5a6a382394e1ff68c
Beorm <abruquerque@gmail.com>
2026-09-22 20:35:10 -0300

    `Adiciona os arquivos as aulas`

--- 3-Estrutura_de_Programação/exercicos/scripts.js
+++ 3-Estrutura_de_Programação/exercicos/scripts.js
@@ -1,84 +1,84 @@
 // exercicios while
-// console.log("________exercicios while______");
-// {
-//   let xx = 0;
-//   let x = 0;
-//   while (xx <= 10) {
-//     console.log("X é igual a " + xx);
-//     xx = xx + 1;
-//   }
+console.log("________exercicios while______");
+{
+  let xx = 0;
+  let x = 0;
+  while (xx <= 10) {
+    console.log("X é igual a " + xx);
+    xx = xx + 1;
+  }
 
-//   let y = 10;
-//   while (y >= 0) {
-//     console.log("Lançamento em " + y);
-//     y = y - 1;
-//   }
+  let y = 10;
+  while (y >= 0) {
+    console.log("Lançamento em " + y);
+    y = y - 1;
+  }
 
-//   while (x <= 20) {
-//     if (x % 2 === 0) console.log("X é igual a " + x);
-//     x = x + 1;
-//   }
+  while (x <= 20) {
+    if (x % 2 === 0) console.log("X é igual a " + x);
+    x = x + 1;
+  }
 
-//   let tabuada = Number(prompt("Digite o valor")),
-//     z = 1;
-//   while (z <= 10) {
-//     console.log(tabuada + " x " + z + " = " + tabuada * z);
-//     z = z + 1;
-//   }
+  let tabuada = Number(prompt("Digite o valor")),
+    z = 1;
+  while (z <= 10) {
+    console.log(tabuada + " x " + z + " = " + tabuada * z);
+    z = z + 1;
+  }
 
-//   let senha_user = "1234";
-//   let senha = prompt("Digite a senha");
+  let senha_user = "1234";
+  let senha = prompt("Digite a senha");
 
-//   while (senha != senha_user) {
-//     console.log("Senha errada");
-//     senha = prompt("Digite a senha novamente ");
-//   }
-//   console.log("senha correta ");
+  while (senha != senha_user) {
+    console.log("Senha errada");
+    senha = prompt("Digite a senha novamente ");
+  }
+  console.log("senha correta ");
 
-//   let contagem = prompt("digite o numero");
-//   let e = 0;
+  let contagem = prompt("digite o numero");
+  let e = 0;
 
-//   while (e <= contagem) {
-//     console.log("lançamento em " + e);
-//     e = e + 1;
-//   }
+  while (e <= contagem) {
+    console.log("lançamento em " + e);
+    e = e + 1;
+  }
 
-//   let soma = 0;
+  let soma = 0;
 
-//   while (true) {
-//     let numero = Number(prompt("Digite o valor"));
-//     if (numero == 0) {
-//       break;
-//     }
-//     soma = soma + numero;
-//   }
-//   console.log("A soma é " + soma);
+  while (true) {
+    let numero = Number(prompt("Digite o valor"));
+    if (numero == 0) {
+      break;
+    }
+    soma = soma + numero;
+  }
+  console.log("A soma é " + soma);
 
-//   while (true) {
-//     let numero = Number(prompt("Digite um número (ou 0 para sair):"));
-//     if (numero === 0) {
-//       console.log("Programa encerrado.");
-//       break;
-//     }
-//     if (numero > 0) {
-//       console.log(numero + " -> Positivo");
-//     } else {
-//       console.log(numero + " -> Negativo");
-//     }
-//   }
-//   let quantidadePositiva = 0;
-//   while (true) {
-//     let contgemPo = Number(
-//       prompt("digite os valores(digite 0 para encerrar):"),
-//     );
-//     if (contgemPo === 0) {
-//       break;
-//     } else if (contgemPo > 0) {
-//       quantidadePositiva++; //é o mesmo que quantidadePositiva = quantidadePositiva + 1
-//     }
-//   }
-//   console.log("Você digitou " + quantidadePositiva + " numeros positivos");
-// }
+  while (true) {
+    let numero = Number(prompt("Digite um número (ou 0 para sair):"));
+    if (numero === 0) {
+      console.log("Programa encerrado.");
+      break;
+    }
+    if (numero > 0) {
+      console.log(numero + " -> Positivo");
+    } else {
+      console.log(numero + " -> Negativo");
+    }
+  }
+  let quantidadePositiva = 0;
+  while (true) {
+    let contgemPo = Number(
+      prompt("digite os valores(digite 0 para encerrar):"),
+    );
+    if (contgemPo === 0) {
+      break;
+    } else if (contgemPo > 0) {
+      quantidadePositiva++; //é o mesmo que quantidadePositiva = quantidadePositiva + 1
+    }
+  }
+  console.log("Você digitou " + quantidadePositiva + " numeros positivos");
+}
 // exercicios for
 console.log("________exercicios for______");
 {
@@ -90,9 +90,9 @@ console.log("________exercicios for______");
     console.log(`Lançamento em ${i}`);
   }
 
-  // for (let i = 0; i <= 20; i = i + 2) {
-  //   console.log(`é ${i}`);
-  // }
+  for (let i = 0; i <= 20; i = i + 2) {
+    console.log(`é ${i}`);
+  }
 
   for (let i = 0; i <= 20; i = i + 1) {
     if (i % 2 === 0) console.log(`Agora é ${i}`);
@@ -106,11 +106,11 @@ console.log("________exercicios for______");
   for (let i = 0; i <= 10; i = i + 1) {
     console.log(tabuada + " x " + i + " = " + tabuada * i);
   }
-  // let soma = 0;
-  // for (let i = 0; i <= 100; i = i + 1) {
-  //   soma = soma + i;
-  //   console.log(`A soma é ${soma}`);
-  // }
+  let soma1 = 0;
+  for (let i = 0; i <= 100; i = i + 1) {
+    soma1 = soma1 + i;
+    console.log(`A soma é ${soma1}`);
+  }
 
   for (i = 0; i <= 100; i = i + 1) {
     if (i % 5 === 0) console.log(`${i} divisivel 5`);
